47fdf9c9d42ff6adcadbe73e477c6097505556df
Nimrod León <nleonc14@gmail.com>
2021-05-29 18:24:21 -0500

    componente artículos

--- packages/express/src/network/network.js
+++ packages/express/src/network/network.js
@@ -1,3 +1,4 @@
 export * from '../components/auth/network'
 export * from '../components/warehouse/network'
 export * from '../components/taxes/network'
+export * from '../components/article/network'
--- packages/express/src/network/routes.js
+++ packages/express/src/network/routes.js
@@ -4,6 +4,7 @@ const routes = (server) => {
   server.use('/api/users', network.authRouter)
   server.use('/api/warehouses', network.warehouseRouter)
   server.use('/api/taxes', network.taxRouter)
+  server.use('/api/articles', network.articleRouter)
 }
 
 export default routes
