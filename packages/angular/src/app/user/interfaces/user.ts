bdcfe62a6061d06303b25ba67e03527e9cc373f9
Nimrod León <nleonc14@gmail.com>
2021-06-16 19:21:13 -0500

    user module generated

---
+++ packages/angular/src/app/user/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id: string,
+  fullName: string,
+  address: string,
+  phoneNumber: string,
+  userName: string,
+  password: string,
+  permission: string,
+  email: string,
+}
