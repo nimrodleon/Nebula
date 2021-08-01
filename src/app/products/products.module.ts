0ae5cf60d0457b7d7ae42245ca5efeca8ba09048
Geison E. León <gleonc09@outlook.es>
2021-08-01 00:01:24 -0500

    product-modal generated

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,11 +5,13 @@ import {ProductsRoutingModule} from './products-routing.module';
 import {ProductListComponent} from './pages/product-list/product-list.component';
 import {GlobalModule} from '../global/global.module';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { ProductModalComponent } from './components/product-modal/product-modal.component';
 
 
 @NgModule({
   declarations: [
-    ProductListComponent
+    ProductListComponent,
+    ProductModalComponent
   ],
   imports: [
     CommonModule,
