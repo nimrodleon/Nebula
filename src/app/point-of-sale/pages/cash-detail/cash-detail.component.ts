1772efbac216a0ae15c9eca7038245f75bee1210
Nimrod León <nleonc14@gmail.com>
2021-09-13 19:16:21 -0500

    HTML modal entrada/salida de efectivo

--- src/app/point-of-sale/pages/cash-detail/cash-detail.component.ts
+++ src/app/point-of-sale/pages/cash-detail/cash-detail.component.ts
@@ -2,14 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {
   faCog,
   faLock, faMinusCircle,
-  faPlus,
-  faPrint, faSearch,
-  faSignOutAlt,
-  faSyncAlt,
-  faTimes,
-  faTimesCircle,
+  faPlus, faSearch,
   faTrashAlt
 } from '@fortawesome/free-solid-svg-icons';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-cash-detail',
@@ -17,20 +14,28 @@ import {
   styleUrls: ['./cash-detail.component.scss']
 })
 export class CashDetailComponent implements OnInit {
-  faSyncAlt = faSyncAlt;
   faPlus = faPlus;
   faTrashAlt = faTrashAlt;
-  faSignOutAlt = faSignOutAlt;
   faLock = faLock;
-  faPrint = faPrint;
   faMinusCircle = faMinusCircle;
   faSearch = faSearch;
-  faCog=faCog;
+  faCog = faCog;
+  // ========================================
+  cashInOutModal: any;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    // modal entrada/salida de efectivo.
+    this.cashInOutModal = new bootstrap.Modal(
+      document.querySelector('#app-cash-in-out-modal'));
+  }
+
+  // Abrir modal entrada/salida de efectivo.
+  addCashInOut(e: any): void {
+    e.preventDefault();
+    this.cashInOutModal.show();
   }
 
 }
--- src/app/point-of-sale/point-of-sale.module.ts
+++ src/app/point-of-sale/point-of-sale.module.ts
@@ -6,11 +6,13 @@ import {PointOfSaleRoutingModule} from './point-of-sale-routing.module';
 import {GlobalModule} from '../global/global.module';
 import {CashDetailComponent} from './pages/cash-detail/cash-detail.component';
 import {CajaDiariaComponent} from './pages/caja-diaria/caja-diaria.component';
+import {CashInOutModalComponent} from './components/cash-in-out-modal/cash-in-out-modal.component';
 
 @NgModule({
   declarations: [
     CajaDiariaComponent,
     CashDetailComponent,
+    CashInOutModalComponent,
   ],
   imports: [
     CommonModule,
