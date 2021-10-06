efa74aee26639477e88753b36ad526749fa3acc5
Nimrod León <nleonc14@gmail.com>
2021-10-06 16:45:06 -0500

    terminal component created!

--- src/app/point-of-sale/point-of-sale.module.ts
+++ src/app/point-of-sale/point-of-sale.module.ts
@@ -7,12 +7,14 @@ import {GlobalModule} from '../global/global.module';
 import {CashDetailComponent} from './pages/cash-detail/cash-detail.component';
 import {CajaDiariaComponent} from './pages/caja-diaria/caja-diaria.component';
 import {CashInOutModalComponent} from './components/cash-in-out-modal/cash-in-out-modal.component';
+import {TerminalComponent} from './pages/terminal/terminal.component';
 
 @NgModule({
   declarations: [
     CajaDiariaComponent,
     CashDetailComponent,
     CashInOutModalComponent,
+    TerminalComponent,
   ],
   imports: [
     CommonModule,
