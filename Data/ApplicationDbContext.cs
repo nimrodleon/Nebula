d392510ede0e1ee102a5bf29e90ad27b973ed477
Nimrod León <nleonc14@gmail.com>
2021-10-18 16:04:34 -0500

    refactorización ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace Nebula.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
