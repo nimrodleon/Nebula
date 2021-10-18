781ad5a473ed422cc78b37c05b602fba7a2e18a1
Nimrod León <nleonc14@gmail.com>
2021-10-18 16:13:26 -0500

    user model migration generated

---
+++ Migrations/20211018211034_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Nebula.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    UserName = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    Password = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    Role = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    Email = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: true),
+                    Suspended = table.Column<bool>(type: "boolean", nullable: false),
+                    SoftDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
