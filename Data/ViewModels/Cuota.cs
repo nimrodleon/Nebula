f95fa973003f37066f5b416eb616d13d45f55655
Geison Eliezer León <gleonc09@gmail.com>
2021-11-07 00:57:44 -0500

    viewModel ventas

---
+++ Data/ViewModels/Cuota.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Nebula.Data.ViewModels
+{
+    public class Cuota
+    {
+        public int NumCuota { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Data/ViewModels/SaleDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace Nebula.Data.ViewModels
+{
+    public class SaleDetail
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
