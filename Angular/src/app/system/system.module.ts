18dcd6c6e28407a1adf84a696e003fba7e618a03
Nimrod León <nleonc14@gmail.com>
2021-12-01 19:18:16 -0500

    refactorización import fontAwesome

--- Angular/src/app/system/system.module.ts
+++ Angular/src/app/system/system.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import {SystemRoutingModule} from './system-routing.module';
 import {CompanyComponent} from './pages/company/company.component';
 import {GlobalModule} from '../global/global.module';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
