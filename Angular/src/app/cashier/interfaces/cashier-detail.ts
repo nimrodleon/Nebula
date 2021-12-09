d8a7017fde8a8cd6651d8c21784a3cd4500cdeeb
Geison Eliezer León <gleonc09@gmail.com>
2021-12-08 23:30:47 -0500

    refactorización detalle de caja

--- Angular/src/app/cashier/interfaces/cashier-detail.ts
+++ Angular/src/app/cashier/interfaces/cashier-detail.ts
@@ -1,24 +1,12 @@
-export class CashierDetail {
-  id: number | null | undefined;
-  cajaDiariaId: number | null;
-  invoiceId: number | null;
-  typeOperation: number | null;
+export interface CashierDetail {
+  id: number;
+  cajaDiariaId: number;
+  invoiceId: number;
+  typeOperation: number;
   startDate: any;
   document: string;
   contact: string;
   glosa: string;
   type: string;
   total: number;
-
-  constructor() {
-    this.id = null;
-    this.cajaDiariaId = null;
-    this.invoiceId = null;
-    this.typeOperation = null;
-    this.document = '';
-    this.contact = '';
-    this.glosa = '';
-    this.type = '';
-    this.total = 0;
-  }
 }
