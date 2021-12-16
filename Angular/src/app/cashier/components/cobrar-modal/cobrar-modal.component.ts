5ad7a5754dfe15fba1db832980d12ae9267dc64f
Nimrod León <nleonc14@gmail.com>
2021-12-16 15:15:58 -0500

    refactorización plantilla cobrar modal

--- Angular/src/app/cashier/components/cobrar-modal/cobrar-modal.component.ts
+++ Angular/src/app/cashier/components/cobrar-modal/cobrar-modal.component.ts
@@ -2,11 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {faBars, faEnvelope, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import {faCheckSquare} from '@fortawesome/free-regular-svg-icons';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {deleteConfirm} from 'src/app/global/interfaces';
 import {TerminalService} from '../../services';
-import {Cuota} from '../../interfaces';
-
-declare var bootstrap: any;
 
 @Component({
   selector: 'app-cobrar-modal',
@@ -21,16 +17,12 @@ export class CobrarModalComponent implements OnInit {
   // ====================================================================================================
   @Input()
   cajaDiariaId: number = 0;
-  cuotaModal: any;
   cobrarForm: FormGroup = this.fb.group({
-    paymentType: ['Contado'],
-    docType: ['NV'],
-    endDate: [null],
-    montoCaja: [true],
+    paymentType: ['0'],
+    docType: ['BL'],
     montoTotal: [0],
     remark: ['']
   });
-  listaDeCuotas: Array<Cuota> = new Array<Cuota>();
   formReg: boolean = true;
 
   constructor(
@@ -39,7 +31,6 @@ export class CobrarModalComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.cuotaModal = new bootstrap.Modal(document.querySelector('#cuota-modal'));
     const myModal: any = document.querySelector('#cobrar-modal');
     myModal.addEventListener('hide.bs.modal', () => {
       if (!this.formReg) {
@@ -53,45 +44,8 @@ export class CobrarModalComponent implements OnInit {
     return this.terminalService.sale;
   }
 
-  // agregar cuota.
-  public addCuotaModal(e: any): void {
-    e.preventDefault();
-    this.cuotaModal.show();
-  }
-
-  // ocultar cuota modal.
-  public hideCuotaModal(data: Cuota): void {
-    if (data) {
-      data.numCuota = this.listaDeCuotas.length + 1;
-      this.listaDeCuotas.push(data);
-      this.cuotaModal.hide();
-    }
-  }
-
-  // borrar item lista de cuotas.
-  public deleteItemCuota(numCuota: number): void {
-    deleteConfirm().then(result => {
-      if (result.isConfirmed) {
-        let deleted: Boolean = false;
-        this.listaDeCuotas.forEach((value, index, array) => {
-          if (value.numCuota === numCuota) {
-            array.splice(index, 1);
-            deleted = true;
-          }
-        });
-        if (deleted) {
-          for (let i = 0; i < this.listaDeCuotas.length; i++) {
-            this.listaDeCuotas[i].numCuota = i + 1;
-          }
-        }
-      }
-    });
-  }
-
-  // cobrar venta.
   public cobrarVenta(): void {
     this.terminalService.addInfo(this.cobrarForm.value);
-    this.terminalService.addCuotas(this.listaDeCuotas);
     this.terminalService.saveChanges(this.cajaDiariaId).subscribe(result => {
       if (result.ok) {
         if (result.data) {
