d1d12bc3838880acc5f11ed107a8a313f74030c6
Nimrod León <nleonc14@gmail.com>
2021-12-30 16:25:15 -0500

    refactoring entrada/salida de dinero

--- Angular/src/app/cashier/components/cash-in-out-modal/cash-in-out-modal.component.ts
+++ Angular/src/app/cashier/components/cash-in-out-modal/cash-in-out-modal.component.ts
@@ -1,11 +1,11 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {environment} from 'src/environments/environment';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {environment} from 'src/environments/environment';
 import {CashierDetail} from '../../interfaces';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {ContactService} from '../../../contact/services';
+import {ContactService} from 'src/app/contact/services';
 import {CashierDetailService} from '../../services';
-import {ResponseData} from '../../../global/interfaces';
+import {ResponseData} from 'src/app/global/interfaces';
 
 declare var jQuery: any;
 
@@ -21,14 +21,13 @@ export class CashInOutModalComponent implements OnInit {
   cajaDiariaId: number = 0;
   @Output()
   responseData = new EventEmitter<ResponseData<CashierDetail>>();
-  cashierDetail: CashierDetail | any;
+  cashierDetail: CashierDetail = new CashierDetail();
   cajaChicaForm: FormGroup = this.fb.group({
     id: [null],
-    cajaDiariaId: [''],
-    contact: [''],
-    glosa: [''],
-    type: [''],
-    total: 0,
+    contactId: [null, [Validators.required]],
+    glosa: ['', [Validators.required]],
+    type: ['ENTRADA', [Validators.required]],
+    total: [0, [Validators.required, Validators.min(0.1)]],
   });
 
   constructor(
@@ -48,25 +47,36 @@ export class CashInOutModalComponent implements OnInit {
           Authorization: 'Bearer ' + localStorage.getItem('token')
         }
       }
+    }).on('select2:select', (e: any) => {
+      const data = e.params.data;
+      this.cajaChicaForm.controls['contactId'].setValue(data.id);
     });
-    // suscribir cambios del formulario.
-    this.cajaChicaForm.valueChanges
-      .subscribe(value => this.cashierDetail = value);
     // cargar valores por defecto.
     if (document.querySelector('#cash-in-out-modal')) {
       const myModal: any = document.querySelector('#cash-in-out-modal');
       myModal.addEventListener('shown.bs.modal', () => {
         jQuery('#contactId').val(null).trigger('change');
-        this.cashierDetail = null;
+        this.cashierDetail = new CashierDetail();
         this.cashierDetail.cajaDiariaId = <any>this.cajaDiariaId;
-        this.cajaChicaForm.reset({...this.cashierDetail});
+        this.cajaChicaForm.reset();
       });
     }
   }
 
+  // Verificar campo invalido.
+  public inputIsInvalid(field: string) {
+    return this.cajaChicaForm.controls[field].errors && this.cajaChicaForm.controls[field].touched;
+  }
+
   // guardar cambios.
   public saveChanges(): void {
-    const contactId = jQuery('#contactId').val();
+    if (this.cajaChicaForm.invalid) {
+      this.cajaChicaForm.markAllAsTouched();
+      return;
+    }
+    // Guardar datos, sólo si es válido el formulario.
+    const contactId = this.cajaChicaForm.get('contactId')?.value;
+    this.cashierDetail = {...this.cajaChicaForm.value};
     this.contactService.show(contactId).subscribe(result => {
       this.cashierDetail.id = undefined;
       this.cashierDetail.cajaDiariaId = <any>this.cajaDiariaId;
--- Angular/src/app/cashier/interfaces/cashier-detail.ts
+++ Angular/src/app/cashier/interfaces/cashier-detail.ts
@@ -1,13 +1,15 @@
-export interface CashierDetail {
-  id: number;
-  cajaDiariaId: number;
-  invoiceId: number;
-  typeOperation: number;
-  startDate: any;
-  document: string;
-  contact: string;
-  glosa: string;
-  paymentMethod: number;
-  type: string;
-  total: number;
+export class CashierDetail {
+  constructor(
+    public id: number | any = null, // identificador clave primaria.
+    public cajaDiariaId: number | any = null, // identificador de caja diaria.
+    public invoiceId: number | any = null, // identificador del comprobante de venta.
+    public typeOperation: number = 0, // tipo operación: <Caja Chica/Comprobante>
+    public startDate: any = null, // fecha de registro.
+    public document: string = '', // serie y número del comprobante.
+    public contact: string = '', // nombre de contacto.
+    public glosa: string = '', // observación de la operación.
+    public paymentMethod: number = 0, // medios de pago: <dinero en efectivo, depósito en cuenta, billetera digital>
+    public type: string = 'ENTRADA', // tipo de operación: <ENTRADA/SALIDA>
+    public total: number = 0,) {
+  }
 }
