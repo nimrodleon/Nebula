fa75b2be88645b5be014b6b9a9d135692cd54d0f
Nimrod León <nleonc14@gmail.com>
2022-01-17 18:21:11 -0500

    validation contact-modal.component.html refactoring

--- Angular/src/app/contact/components/contact-modal/contact-modal.component.ts
+++ Angular/src/app/contact/components/contact-modal/contact-modal.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
 import {Contact} from '../../interfaces';
 import {ContactService} from '../../services';
@@ -22,12 +22,12 @@ export class ContactModalComponent implements OnInit {
   // ====================================================================================================
   contactForm: FormGroup = this.fb.group({
     id: [null],
-    document: [''],
-    docType: [0],
-    name: [''],
-    address: [''],
-    phoneNumber: [''],
-    email: [''],
+    document: ['', [Validators.required]],
+    docType: [0, [Validators.required]],
+    name: ['', [Validators.required]],
+    address: ['', [Validators.required]],
+    phoneNumber: ['', [Validators.required]],
+    email: ['', [Validators.required]],
   });
 
   constructor(
@@ -43,11 +43,24 @@ export class ContactModalComponent implements OnInit {
       myModal.addEventListener('shown.bs.modal', () => {
         this.contactForm.reset({...this.contact});
       });
+      myModal.addEventListener('hide.bs.modal', () => {
+        this.contactForm.reset();
+      });
     }
+  }
+
+  // Verificar campo invalido.
+  public inputIsInvalid(field: string) {
+    return this.contactForm.controls[field].errors && this.contactForm.controls[field].touched;
   }
 
   // guardar cambios del formulario.
   public saveChanges(): void {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+    // Guardar datos, sólo si es válido el formulario.
     if (this.contactForm.get('id')?.value == null) {
       this.contact.id = undefined;
       this.contactService.create(this.contact)
