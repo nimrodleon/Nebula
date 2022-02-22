0278105243ac04df200c97d31f15fc85cd848073
Geison Eliezer León <gleonc09@gmail.com>
2022-02-22 00:02:17 -0500

    crud básico categorías

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Nebula.Data;
 using Nebula.Data.Models;
-using Nebula.Data.ViewModels;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
 
 namespace Nebula.Controllers
 {
@@ -13,9 +11,9 @@ namespace Nebula.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IRavenDbContext _context;
 
-        public CategoryController(ApplicationDbContext context)
+        public CategoryController(IRavenDbContext context)
         {
             _context = context;
         }
@@ -23,75 +21,81 @@ namespace Nebula.Controllers
         [HttpGet("Index")]
         public async Task<IActionResult> Index([FromQuery] string query)
         {
-            var result = from m in _context.Categories select m;
+            using var session = _context.Store.OpenAsyncSession();
+            IRavenQueryable<Category> categories = from m in session.Query<Category>() select m;
             if (!string.IsNullOrWhiteSpace(query))
-                result = result.Where(m => m.Name.ToLower().Contains(query.ToLower()));
-            result = result.OrderByDescending(m => m.Id);
-            var responseData = await result.AsNoTracking().Take(25).ToListAsync();
+                categories = categories.Search(m => m.Name, $"*{query.ToUpper()}*");
+            categories = categories.OrderByDescending(m => m.Id);
+            var responseData = await categories.Take(25).ToListAsync();
             return Ok(responseData);
         }
 
         [HttpGet("Show/{id}")]
-        public async Task<IActionResult> Show(int id)
+        public async Task<IActionResult> Show(string id)
         {
-            var result = await _context.Categories.AsNoTracking()
-                .FirstOrDefaultAsync(m => m.Id.Equals(id));
-            return Ok(result);
+            using var session = _context.Store.OpenAsyncSession();
+            Category category = await session.LoadAsync<Category>(id);
+            return Ok(category);
         }
 
-        [HttpGet("Select2")]
-        public async Task<IActionResult> Select2([FromQuery] string term)
-        {
-            var result = from m in _context.Categories select m;
-            if (!string.IsNullOrWhiteSpace(term))
-                result = result.Where(m => m.Name.ToLower().Contains(term.ToLower()));
-            result = result.OrderByDescending(m => m.Id);
-            var responseData = await result.AsNoTracking().Take(10).ToListAsync();
-            var data = new List<Select2>();
-            responseData.ForEach(item =>
-            {
-                data.Add(new Select2()
-                {
-                    Id = item.Id, Text = item.Name
-                });
-            });
-            return Ok(new {Results = data});
-        }
+        // [HttpGet("Select2")]
+        // public async Task<IActionResult> Select2([FromQuery] string term)
+        // {
+        //     var result = from m in _context.Categories select m;
+        //     if (!string.IsNullOrWhiteSpace(term))
+        //         result = result.Where(m => m.Name.ToLower().Contains(term.ToLower()));
+        //     result = result.OrderByDescending(m => m.Id);
+        //     var responseData = await result.AsNoTracking().Take(10).ToListAsync();
+        //     var data = new List<Select2>();
+        //     responseData.ForEach(item =>
+        //     {
+        //         data.Add(new Select2()
+        //         {
+        //             Id = item.Id, Text = item.Name
+        //         });
+        //     });
+        //     return Ok(new {Results = data});
+        // }
 
         [HttpPost("Create")]
         public async Task<IActionResult> Create([FromBody] Category model)
         {
+            using var session = _context.Store.OpenAsyncSession();
             model.Name = model.Name.ToUpper();
-            _context.Categories.Add(model);
-            await _context.SaveChangesAsync();
+            await session.StoreAsync(model);
+            await session.SaveChangesAsync();
+
             return Ok(new
             {
                 Ok = true, Data = model,
-                Msg = $"{model.Name} ha sido registrado!"
+                Msg = $"La Categoría {model.Name} ha sido registrado!"
             });
         }
 
         [HttpPut("Update/{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] Category model)
+        public async Task<IActionResult> Update(string id, [FromBody] Category model)
         {
             if (id != model.Id) return BadRequest();
-            _context.Categories.Update(model);
-            await _context.SaveChangesAsync();
+            using var session = _context.Store.OpenAsyncSession();
+            Category category = await session.LoadAsync<Category>(id);
+            category.Name = model.Name.ToUpper();
+            await session.SaveChangesAsync();
+
             return Ok(new
             {
-                Ok = true, Data = model,
-                Msg = $"{model.Name} ha sido actualizado!"
+                Ok = true, Data = category,
+                Msg = $"La Categoría {category.Name} ha sido actualizado!"
             });
         }
 
         [HttpDelete("Delete/{id}")]
-        public async Task<IActionResult> Delete(int id)
+        public async Task<IActionResult> Delete(string id)
         {
-            var result = await _context.Categories.FirstOrDefaultAsync(m => m.Id.Equals(id));
-            if (result == null) return BadRequest();
-            _context.Categories.Remove(result);
-            await _context.SaveChangesAsync();
-            return Ok(new {Ok = true, Data = result, Msg = "La categoría ha sido borrado!"});
+            using var session = _context.Store.OpenAsyncSession();
+            Category category = await session.LoadAsync<Category>(id);
+            session.Delete(category);
+            await session.SaveChangesAsync();
+            return Ok(new {Ok = true, Data = category, Msg = "La categoría ha sido borrado!"});
         }
     }
 }
