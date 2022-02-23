7ac73a653c97fadc95f41927059c5d0b6a74c388
Geison Eliezer León <gleonc09@gmail.com>
2022-02-22 23:43:30 -0500

    refactoring product model

--- Angular/src/app/products/interfaces/product.ts
+++ Angular/src/app/products/interfaces/product.ts
@@ -1,6 +1,6 @@
 export class Product {
   constructor(
-    public id: number | any = null,
+    public id: any = undefined,
     public description: string = '',
     public barcode: string = '',
     public price1: number = 0,
@@ -8,7 +8,7 @@ export class Product {
     public fromQty: number = 0,
     public igvSunat: string = '',
     public icbper: string = '',
-    public categoryId: number | any = null,
+    public category: string = '',
     public undMedida: string = '',
     public type: string = '',
     public pathImage: string = '') {
