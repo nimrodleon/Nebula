ad5c7116c3e76d2ac5dbb9714b563b763f33bca3
Nimrod León <nleonc14@gmail.com>
2022-03-05 18:29:55 -0500

    avance testing

---
+++ src/WebApp/src/app/cashier/pages/terminal/terminal.component.spec.ts
@@ -0,0 +1,27 @@
+﻿import {TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {TerminalComponent} from './terminal.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('TerminalComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        TerminalComponent
+      ]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(TerminalComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
