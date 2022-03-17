c88d6b0eb98f4150522a503628f07ed91fae6f19
Nimrod León <nleonc14@gmail.com>
2022-03-16 19:24:43 -0500

    avance migración

--- src/Nebula/Program.cs
+++ src/Nebula/Program.cs
@@ -1,26 +1,37 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Nebula.Data;
+using Nebula.Data.Services;
 
-namespace Nebula
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<PersistenceSettings>(builder.Configuration.GetSection("RavenDb"));
+
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddPolicy(name: "MyPolicy", builder =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Add services to the container.
+builder.Services.AddSingleton<IRavenDbContext, RavenDbContext>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddScoped<ICpeService, CpeService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
