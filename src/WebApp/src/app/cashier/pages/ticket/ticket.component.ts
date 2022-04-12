54a2a6f758752c4d81a578ade9b8baa4d8f43504
Nimrod León <nleonc14@gmail.com>
2022-04-11 19:14:03 -0500

    ticket de venta caja

--- src/WebApp/src/app/cashier/pages/ticket/ticket.component.ts
+++ src/WebApp/src/app/cashier/pages/ticket/ticket.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ResponseInvoiceSale} from 'src/app/sales/interfaces';
+import {InvoiceSaleService} from 'src/app/sales/services';
+import {ConfigurationService} from 'src/app/system/services';
+import {Configuration} from 'src/app/system/interfaces';
 
 @Component({
   selector: 'app-ticket',
-  templateUrl: './ticket.component.html',
-  styleUrls: ['./ticket.component.scss']
+  templateUrl: './ticket.component.html'
 })
 export class TicketComponent implements OnInit {
+  configuration: Configuration = new Configuration();
+  responseInvoiceSale: ResponseInvoiceSale = new ResponseInvoiceSale();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private configurationService: ConfigurationService,
+    private invoiceSaleService: InvoiceSaleService) {
   }
 
+  ngOnInit(): void {
+    const id: string = this.activatedRoute.snapshot.params['id'];
+    this.invoiceSaleService.show(id).subscribe(result => this.responseInvoiceSale = result);
+    this.configurationService.show().subscribe(result => this.configuration = result);
+  }
+
+  public print(): void {
+    window.print();
+  }
 }
