30b1d0508674b09d551cf575e62118f34d05cb40
Nimrod León <nleonc14@gmail.com>
2022-08-11 18:02:37 -0500

    modelo de ubicaciones

---
+++ Database/Models/Inventory/Location.cs
@@ -0,0 +1,26 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nebula.Database.Models.Inventory;
+
+public class Location : Generic
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador del Almacén.
+    /// </summary>
+    public string WarehouseId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre del Almacén.
+    /// </summary>
+    public string WarehouseName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Descripción de la Ubicación.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}
