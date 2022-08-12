7edf26bc3008d74bf447260ced50de1e9e111b8f
Nimrod León <nleonc14@gmail.com>
2022-08-12 18:56:37 -0500

    avance contact select

---
+++ Database/ViewModels/Common/ContactSelect.cs
@@ -0,0 +1,10 @@
+namespace Nebula.Database.ViewModels.Common;
+
+public class ContactSelect
+{
+    public string Id { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string DocType { get; set; } = string.Empty;
+    public string Document { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
