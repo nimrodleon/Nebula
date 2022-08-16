07144148cdb644eac63060ea0261de96c23505df
Nimrod León <nleonc14@gmail.com>
2022-08-16 17:40:36 -0500

    controlador inventario entrada / salida

---
+++ Controllers/Inventory/InventoryNotasController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nebula.Database.Helpers;
+using Nebula.Database.Models.Inventory;
+using Nebula.Database.Services.Inventory;
+using Nebula.Database.ViewModels.Common;
+
+namespace Nebula.Controllers.Inventory;
+
+[Authorize(Roles = AuthRoles.User)]
+[Route("api/[controller]")]
+[ApiController]
+public class InventoryNotasController : ControllerBase
+{
+    private readonly InventoryNotasService _inventoryNotasService;
+
+    public InventoryNotasController(InventoryNotasService inventoryNotasService)
+    {
+        _inventoryNotasService = inventoryNotasService;
+    }
+
+    [HttpGet("Index")]
+    public async Task<IActionResult> Index([FromQuery] DateQuery model)
+    {
+        var responseData = await _inventoryNotasService.GetListAsync(model);
+        return Ok(responseData);
+    }
+
+    [HttpGet("Show/{id}")]
+    public async Task<IActionResult> Show(string id)
+    {
+        var inventoryNotas = await _inventoryNotasService.GetAsync(id);
+        return Ok(inventoryNotas);
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create([FromBody] InventoryNotas model)
+    {
+        var inventoryNotas = await _inventoryNotasService.CreateAsync(model);
+        return Ok(inventoryNotas);
+    }
+
+    [HttpPut("Update/{id}")]
+    public async Task<IActionResult> Update(string id, [FromBody] InventoryNotas model)
+    {
+        var inventoryNotas = await _inventoryNotasService.GetAsync(id);
+        model.Id = inventoryNotas.Id;
+        var responseData = await _inventoryNotasService.UpdateAsync(id, model);
+        return Ok(responseData);
+    }
+
+    [HttpDelete("Delete/{id}"), Authorize(Roles = AuthRoles.Admin)]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var inventoryNotas = await _inventoryNotasService.GetAsync(id);
+        await _inventoryNotasService.RemoveAsync(inventoryNotas.Id);
+        return Ok(inventoryNotas);
+    }
+}
