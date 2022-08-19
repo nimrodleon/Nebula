a713f21eeb1350a1fa60e279678f8091b07264cf
Nimrod León <nleonc14@gmail.com>
2022-08-19 11:53:19 -0500

    modelo ajuste de inventario

---
+++ Database/Models/Inventory/AjusteInventarioDetail.cs
@@ -0,0 +1,41 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Nebula.Database.Models.Inventory;
+
+public class AjusteInventarioDetail
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Clave foranea Ajuste Inventario.
+    /// </summary>
+    public string AjusteInventarioId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador del Producto.
+    /// </summary>
+    public string ProductId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre del Producto.
+    /// </summary>
+    public string ProductName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Cantidad Existente.
+    /// </summary>
+    public long CantExistente { get; set; }
+
+    /// <summary>
+    /// Cantidad Contada.
+    /// </summary>
+    public long CantContada { get; set; }
+
+    /// <summary>
+    /// Cantidad Restante.
+    /// </summary>
+    public long CantRestante { get; set; }
+}
