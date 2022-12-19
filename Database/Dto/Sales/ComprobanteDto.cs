abf34ebdccd428074cd1f83d0a53a18c6b4de1d7
Geison Eliezer León <gleonc09@gmail.com>
2022-12-19 04:11:13 -0500

    refactoring comprobanteDto

--- Database/Dto/Sales/ComprobanteDto.cs
+++ Database/Dto/Sales/ComprobanteDto.cs
@@ -6,59 +6,6 @@ namespace Nebula.Database.Dto.Sales;
 
 public class ComprobanteDto
 {
-    struct ImporteItem
-    {
-        /// <summary>
-        /// Valor Unitario sin Ningún Tributo.
-        /// </summary>
-        public decimal MtoValorUnitario { get; set; }
-        /// <summary>
-        /// Sumatoria Tributos por item, = 9 + 16 + 23
-        /// </summary>
-        public decimal SumTotTributosItem { get; set; }
-        /// <summary>
-        /// Monto de IGV por ítem.
-        /// </summary>
-        public decimal MtoIgvItem { get; set; }
-        /// <summary>
-        /// Base Imponible IGV por Item.
-        /// Siempre mayor a cero. Si el item tiene ISC, agregar dicho monto a la base imponible.
-        /// </summary>
-        public decimal MtoBaseIgvItem { get; set; }
-        /// <summary>
-        /// Monto de tributo ICBPER por iItem.
-        /// </summary>
-        public decimal MtoTriIcbperItem { get; set; }
-        /// <summary>
-        /// Total Venta Item sin Ningún Tributo.
-        /// </summary>
-        public decimal MtoValorVentaItem { get; set; }
-        /// <summary>
-        /// El mismo valor que ItemComprobante.
-        /// Solo para Generar los Tributos del Comprobante.
-        /// GRAVADO, EXONERADO, INAFECTO.
-        /// </summary>
-        public string IgvSunat { get; set; }
-    }
-    struct ImporteVenta
-    {
-        /// <summary>
-        /// Total Precio de Venta.
-        /// </summary>
-        public decimal SumPrecioVenta { get; set; }
-        /// <summary>
-        /// Total valor de venta.
-        /// </summary>
-        public decimal SumTotValVenta { get; set; }
-        /// <summary>
-        /// Sumatoria Tributos.
-        /// </summary>
-        public decimal SumTotTributos { get; set; }
-        /// <summary>
-        /// Importe total de la venta, cesión en uso o del servicio prestado.
-        /// </summary>
-        public decimal SumImpVenta { get; set; }
-    }
     private Configuration _configuration = new Configuration();
     #region ORIGIN_HTTP_REQUEST!
     public CabeceraComprobanteDto Cabecera { get; set; } = new CabeceraComprobanteDto();
@@ -66,18 +13,18 @@ public class ComprobanteDto
     public DatoPagoComprobanteDto DatoPago { get; set; } = new DatoPagoComprobanteDto();
     public List<CuotaPagoComprobanteDto> DetallePago { get; set; } = new List<CuotaPagoComprobanteDto>();
     #endregion
-    private List<ImporteItem> ImporteItems { get; set; } = new List<ImporteItem>();
+    private List<SumImpItemDto> ImpItemsDto { get; set; } = new List<SumImpItemDto>();
     public void SetConfiguration(Configuration configuration)
     {
         _configuration = configuration;
     }
 
-    private ImporteVenta CalcularImporteVenta()
+    private SumImpVentaDto CalcularImporteVenta()
     {
-        var importeVenta = new ImporteVenta();
+        var sumImpVentaDto = new SumImpVentaDto();
         Detalle.ForEach(item =>
         {
-            ImporteItem itemObj = new ImporteItem();
+            SumImpItemDto itemObj = new SumImpItemDto();
             decimal mtoTotalItem = item.CtdUnidadItem * item.MtoPrecioVentaUnitario;
             decimal porcentajeIGV = item.IgvSunat == TipoIGV.Gravado ? (_configuration.PorcentajeIgv / 100) + 1 : 1;
             itemObj.MtoValorVentaItem = mtoTotalItem / porcentajeIGV;
@@ -86,12 +33,12 @@ public class ComprobanteDto
             itemObj.MtoIgvItem = mtoTotalItem - itemObj.MtoBaseIgvItem;
             itemObj.SumTotTributosItem = itemObj.MtoIgvItem + itemObj.MtoTriIcbperItem; // el sistema soporta solo IGV/ICBPER.
             itemObj.MtoValorUnitario = itemObj.MtoValorVentaItem / item.CtdUnidadItem;
-            importeVenta.SumTotTributos += itemObj.SumTotTributosItem;
-            importeVenta.SumTotValVenta += itemObj.MtoValorVentaItem;
-            importeVenta.SumPrecioVenta += mtoTotalItem + itemObj.MtoTriIcbperItem;
-            importeVenta.SumImpVenta = importeVenta.SumTotValVenta + importeVenta.SumTotTributos;
+            sumImpVentaDto.SumTotTributos += itemObj.SumTotTributosItem;
+            sumImpVentaDto.SumTotValVenta += itemObj.MtoValorVentaItem;
+            sumImpVentaDto.SumPrecioVenta += mtoTotalItem + itemObj.MtoTriIcbperItem;
+            sumImpVentaDto.SumImpVenta = sumImpVentaDto.SumTotValVenta + sumImpVentaDto.SumTotTributos;
         });
-        return importeVenta;
+        return sumImpVentaDto;
     }
 
     public InvoiceSale GetInvoiceSale()
@@ -151,17 +98,17 @@ public class ComprobanteDto
                     codTipTributoIgvItem = "FRE";
                     break;
             }
-            ImporteItem importeItem = new ImporteItem();
-            importeItem.IgvSunat = item.IgvSunat;
+            SumImpItemDto impItemDto = new SumImpItemDto();
+            impItemDto.IgvSunat = item.IgvSunat;
             decimal mtoTotalItem = item.CtdUnidadItem * item.MtoPrecioVentaUnitario;
             decimal porcentajeIGV = item.IgvSunat == TipoIGV.Gravado ? (_configuration.PorcentajeIgv / 100) + 1 : 1;
-            importeItem.MtoValorVentaItem = mtoTotalItem / porcentajeIGV;
-            importeItem.MtoTriIcbperItem = item.TriIcbper ? item.CtdUnidadItem * _configuration.ValorImpuestoBolsa : 0;
-            importeItem.MtoBaseIgvItem = mtoTotalItem / porcentajeIGV;
-            importeItem.MtoIgvItem = mtoTotalItem - importeItem.MtoBaseIgvItem;
-            importeItem.SumTotTributosItem = importeItem.MtoIgvItem + importeItem.MtoTriIcbperItem; // el sistema soporta solo IGV/ICBPER.
-            importeItem.MtoValorUnitario = importeItem.MtoValorVentaItem / item.CtdUnidadItem;
-            ImporteItems.Add(importeItem);
+            impItemDto.MtoValorVentaItem = mtoTotalItem / porcentajeIGV;
+            impItemDto.MtoTriIcbperItem = item.TriIcbper ? item.CtdUnidadItem * _configuration.ValorImpuestoBolsa : 0;
+            impItemDto.MtoBaseIgvItem = mtoTotalItem / porcentajeIGV;
+            impItemDto.MtoIgvItem = mtoTotalItem - impItemDto.MtoBaseIgvItem;
+            impItemDto.SumTotTributosItem = impItemDto.MtoIgvItem + impItemDto.MtoTriIcbperItem; // el sistema soporta solo IGV/ICBPER.
+            impItemDto.MtoValorUnitario = impItemDto.MtoValorVentaItem / item.CtdUnidadItem;
+            ImpItemsDto.Add(impItemDto);
             // agregar items al comprobante.
             invoiceSaleDetails.Add(new InvoiceSaleDetail
             {
@@ -173,26 +120,26 @@ public class ComprobanteDto
                 CodProducto = "-",
                 CodProductoSunat = "-", // refactorizar agregar código de barra.
                 DesItem = item.DesItem,
-                MtoValorUnitario = importeItem.MtoValorUnitario,
-                SumTotTributosItem = importeItem.SumTotTributosItem,
+                MtoValorUnitario = impItemDto.MtoValorUnitario,
+                SumTotTributosItem = impItemDto.SumTotTributosItem,
                 // Tributo: IGV(1000).
                 CodTriIgv = codTriIgv,
-                MtoIgvItem = importeItem.MtoIgvItem,
-                MtoBaseIgvItem = importeItem.MtoBaseIgvItem,
+                MtoIgvItem = impItemDto.MtoIgvItem,
+                MtoBaseIgvItem = impItemDto.MtoBaseIgvItem,
                 NomTributoIgvItem = nomTributoIgvItem,
                 CodTipTributoIgvItem = codTipTributoIgvItem,
                 TipAfeIgv = tipAfeIgv,
                 PorIgvItem = item.IgvSunat == TipoIGV.Gravado ? _configuration.PorcentajeIgv.ToString("N2") : "0.00",
                 // Tributo ICBPER 7152.
                 CodTriIcbper = item.TriIcbper ? "7152" : "-",
-                MtoTriIcbperItem = item.TriIcbper ? importeItem.MtoTriIcbperItem : 0,
+                MtoTriIcbperItem = item.TriIcbper ? impItemDto.MtoTriIcbperItem : 0,
                 CtdBolsasTriIcbperItem = item.TriIcbper ? Convert.ToInt32(item.CtdUnidadItem) : 0,
                 NomTributoIcbperItem = "ICBPER",
                 CodTipTributoIcbperItem = "OTH",
                 MtoTriIcbperUnidad = item.TriIcbper ? _configuration.ValorImpuestoBolsa : 0,
                 // Precio de Venta Unitario.
                 MtoPrecioVentaUnitario = item.MtoPrecioVentaUnitario,
-                MtoValorVentaItem = importeItem.MtoValorVentaItem,
+                MtoValorVentaItem = impItemDto.MtoValorVentaItem,
                 WarehouseId = item.WarehouseId,
             });
         });
@@ -206,7 +153,7 @@ public class ComprobanteDto
         decimal operaciónInafecto = 0;
         decimal mtoTotalIgv = 0;
         decimal mtoTotalIcbper = 0;
-        ImporteItems.ForEach(item =>
+        ImpItemsDto.ForEach(item =>
         {
             mtoTotalIcbper += item.MtoTriIcbperItem;
             switch (item.IgvSunat)
