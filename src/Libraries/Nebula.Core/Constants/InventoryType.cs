554f553d043c346fd3f00189d1728afcdeb7d600
Nimrod <nleonc14@gmail.com>
2023-02-23 19:32:13 -0500

    modelo notas de entrada

---
+++ src/Libraries/Nebula.Core/Constants/InventoryType.cs
@@ -0,0 +1,10 @@
+﻿namespace Nebula.Core.Constants;
+
+/// <summary>
+/// Tipo de Operación de un Inventario.
+/// </summary>
+public static class InventoryType
+{
+    public const string Entrada = "ENTRADA";
+    public const string Salida = "SALIDA";
+}
---
+++ src/Libraries/Nebula.Data/Inventario/Notas/InventoryNotas.cs
@@ -0,0 +1,68 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Nebula.Core.Constants;
+
+namespace Nebula.Data.Inventario.Notas;
+
+[BsonIgnoreExtraElements]
+public class InventoryNotas : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre de Usuario.
+    /// </summary>
+    public string User { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador del Almacén.
+    /// </summary>
+    public string WarehouseId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre del Almacén.
+    /// </summary>
+    public string WarehouseName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador de Contacto.
+    /// </summary>
+    public string ContactId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre de Contacto.
+    /// </summary>
+    public string ContactName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Tipo de Inventario. ENTRADA | SALIDA
+    /// </summary>
+    public string Type { get; set; } = InventoryType.Entrada;
+
+    /// <summary>
+    /// Estado del Inventario.
+    /// </summary>
+    public string Status { get; set; } = InventoryStatus.Borrador;
+
+    /// <summary>
+    /// Observación.
+    /// </summary>
+    public string Remark { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Fecha de Registro.
+    /// </summary>
+    public string CreatedAt { get; set; } = DateTime.Now.ToString("yyyy-MM-dd");
+
+    /// <summary>
+    /// Año de Registro.
+    /// </summary>
+    public string Year { get; set; } = DateTime.Now.ToString("yyyy");
+
+    /// <summary>
+    /// Mes de Registro.
+    /// </summary>
+    public string Month { get; set; } = DateTime.Now.ToString("MM");
+}
