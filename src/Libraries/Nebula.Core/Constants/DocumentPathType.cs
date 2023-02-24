b713f15da0b8ec4e8170a5eb26e61b5ff1611b92
Nimrod <nleonc14@gmail.com>
2023-02-24 11:37:05 -0500

    modelo Invoice boleta|factura

---
+++ src/Libraries/Nebula.Core/Constants/DocumentPathType.cs
@@ -0,0 +1,8 @@
+﻿namespace Nebula.Core.Constants;
+
+public static class DocumentPathType
+{
+    public const string None = "NONE";
+    public const string Control = "CONTROL";
+    public const string Sfs = "SFS";
+}
---
+++ src/Libraries/Nebula.Data/Ventas/Invoice/DetallePagoSale.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nebula.Data.Ventas.Invoice;
+
+/// <summary>
+/// Detalles de la forma de pago al crédito.
+/// </summary>
+[BsonIgnoreExtraElements]
+public class DetallePagoSale : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// foreignKey in db.
+    /// </summary>
+    public string InvoiceSale { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Monto(s) del pago único o de las cuotas.
+    /// </summary>
+    public decimal MtoCuotaPago { get; set; } = 0;
+
+    /// <summary>
+    /// Fecha(s) de vencimiento del pago único o de las cuotas.
+    /// </summary>
+    public string FecCuotaPago { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Tipo de moneda del pago único o de la cuota.
+    /// </summary>
+    public string TipMonedaCuotaPago { get; set; } = string.Empty;
+}
