89324f28d06bda54dd1c85574fdf0279b6775a31
Nimrod <nleonc14@gmail.com>
2023-02-23 19:47:23 -0500

    modelo transferencia entre almacenes

---
+++ src/Libraries/Nebula.Data/Inventario/Transferencia/TransferenciaDetail.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nebula.Data.Inventario.Transferencia;
+
+[BsonIgnoreExtraElements]
+public class TransferenciaDetail : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador TransferenciaId.
+    /// </summary>
+    public string TransferenciaId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador del Producto.
+    /// </summary>
+    public string ProductId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre del Producto.
+    /// </summary>
+    public string ProductName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Cantidad Existente.
+    /// </summary>
+    public long CantExistente { get; set; }
+
+    /// <summary>
+    /// Cantidad Transferido.
+    /// </summary>
+    public long CantTransferido { get; set; }
+
+    /// <summary>
+    /// Cantidad Restante.
+    /// </summary>
+    public long CantRestante { get; set; }
+}
