3fae2628ed19f6bd4913123c529999f9bf8ff995
Nimrod <nleonc14@gmail.com>
2023-02-24 16:03:06 -0500

    modelo serie de facturación

---
+++ src/Libraries/Nebula.Data/Ventas/Common/InvoiceSerie.cs
@@ -0,0 +1,80 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nebula.Data.Ventas.Common;
+
+/// <summary>
+/// Configura las series de facturación.
+/// </summary>
+[BsonIgnoreExtraElements]
+public class InvoiceSerie : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador Serie.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Identificador del Almacén.
+    /// </summary>
+    public string WarehouseId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Nombre del Almacén.
+    /// </summary>
+    public string WarehouseName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Serie Nota de Venta.
+    /// </summary>
+    public string NotaDeVenta { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Contador Nota de Venta.
+    /// </summary>
+    public int CounterNotaDeVenta { get; set; }
+
+    /// <summary>
+    /// Serie Boleta.
+    /// </summary>
+    public string Boleta { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Contador Boleta.
+    /// </summary>
+    public int CounterBoleta { get; set; }
+
+    /// <summary>
+    /// Serie Factura.
+    /// </summary>
+    public string Factura { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Contador Factura.
+    /// </summary>
+    public int CounterFactura { get; set; }
+
+    /// <summary>
+    /// Serie Nota de Crédito Boleta.
+    /// </summary>
+    public string CreditNoteBoleta { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Contador Nota de Crédito Boleta.
+    /// </summary>
+    public int CounterCreditNoteBoleta { get; set; }
+
+    /// <summary>
+    /// Serie Nota de Crédito Factura.
+    /// </summary>
+    public string CreditNoteFactura { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Contador Nota de Crédito Factura.
+    /// </summary>
+    public int CounterCreditNoteFactura { get; set; }
+}
