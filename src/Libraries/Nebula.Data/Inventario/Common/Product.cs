4e55f6540a5e9ed38ac08ece21d1d0abad6e7b41
Nimrod <nleonc14@gmail.com>
2023-02-23 20:01:31 -0500

    avance clases comunes de inventario

---
+++ src/Libraries/Nebula.Data/Inventario/Common/Product.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Nebula.Data.Inventario.Common;
+
+[BsonIgnoreExtraElements]
+public class Product : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+}
