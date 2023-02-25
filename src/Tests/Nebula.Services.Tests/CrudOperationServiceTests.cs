22a6119929bd80bb72a58170288e3c60e25fcf61
Nimrod <nleonc14@gmail.com>
2023-02-25 17:01:50 -0500

    avance test

--- src/Tests/Nebula.Services.Tests/CrudOperationServiceTests.cs
+++ src/Tests/Nebula.Services.Tests/CrudOperationServiceTests.cs
@@ -1,4 +1,19 @@
-﻿namespace Nebula.Services.Tests;
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Nebula.Data;
+using Moq;
+
+namespace Nebula.Services.Tests;
+
+class TestDocument : IGenericDocument
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+}
 
 /// <summary>
 /// Operaciones Crud para cualquier Clase.
@@ -6,5 +21,27 @@
 [TestClass]
 public class CrudOperationServiceTests
 {
+    private readonly CrudOperationService<TestDocument> _service;
 
+    public CrudOperationServiceTests()
+    {
+        Mock<IOptions<DatabaseSettings>> mockOptions = new();
+        mockOptions.Setup(x => x.Value).Returns(new DatabaseSettings()
+        {
+            ConnectionString = "mongodb://localhost:27017",
+            DatabaseName = "testDb"
+        });
+        _service = new CrudOperationService<TestDocument>(mockOptions.Object);
+    }
+
+    [TestMethod]
+    public async Task CreateAsync_ShouldInsertDocument()
+    {
+        // Arrange
+        var testDocument = new TestDocument { Name = "Test" };
+        // Act
+        var result = await _service.CreateAsync(testDocument);
+        // Assert
+        Assert.IsNotNull(result);
+    }
 }
