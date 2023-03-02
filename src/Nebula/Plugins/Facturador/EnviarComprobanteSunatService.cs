53e9749e8d558b6e5d78215c64263a7d27a272bb
Geison Eliezer León <gleonc09@gmail.com>
2023-03-02 06:16:44 -0500

    clase para enviar comprobantes a sunat

---
+++ src/Nebula/Plugins/Facturador/EnviarComprobanteSunatService.cs
@@ -0,0 +1,17 @@
+﻿namespace Nebula.Plugins.Facturador;
+
+/// <summary>
+/// Envia Comprobantes a SUNAT, cada 2 minutos.
+/// </summary>
+public class EnviarComprobanteSunatService : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
