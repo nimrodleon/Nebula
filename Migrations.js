512d74f438c2262cad390f50114de634bbc22289
Nimrod <nleonc14@gmail.com>
2023-03-16 19:01:10 -0500

    script para la creación del usuario admin en mongodb

--- Migrations.js
+++ Migrations.js
@@ -1,3 +1,11 @@
+// creación del usuario admin
+db.User.insertOne({
+    userName: 'admin',
+    email: 'admin@local.pe',
+    passwordHash: 'AGIvkKeU4dv4D1SAoqgxmse5AicrGjmMAOqjRJe4xwuJ1UhS93ZMmiL6SjfUmVWvww==',
+    role: 'Admin'
+})
+
 // migraciones - v1.2.1
 db.Product.updateMany({}, { $rename: { "Price1": "PrecioVentaUnitario" } })
 db.Product.updateMany({}, { $rename: { "Price2": "ValorUnitario" } })
