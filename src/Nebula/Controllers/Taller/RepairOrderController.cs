f3a2ff4ea14b61df7bf41b102f94401cfa4b6c54
Nimrod <nleonc14@gmail.com>
2023-04-11 18:52:12 -0500

    crear orden de reparación

--- src/Nebula/Controllers/Taller/RepairOrderController.cs
+++ src/Nebula/Controllers/Taller/RepairOrderController.cs
@@ -1,11 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Nebula.Database.Helpers;
+using Nebula.Plugins.Taller.Models;
+using Nebula.Plugins.Taller.Services;
 
-namespace Nebula.Controllers.Taller
+namespace Nebula.Controllers.Taller;
+
+[Authorize(Roles = AuthRoles.User)]
+[Route("api/taller/[controller]")]
+[ApiController]
+public class RepairOrderController : ControllerBase
 {
-    [Route("api/taller/[controller]")]
-    [ApiController]
-    public class RepairOrderController : ControllerBase
-    {
+    private readonly TallerRepairOrderService _repairOrderService;
 
+    public RepairOrderController(TallerRepairOrderService repairOrderService)
+    {
+        _repairOrderService = repairOrderService;
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create([FromBody] TallerRepairOrder model)
+    {
+        await _repairOrderService.CreateAsync(model);
+        return Ok(model);
     }
 }
