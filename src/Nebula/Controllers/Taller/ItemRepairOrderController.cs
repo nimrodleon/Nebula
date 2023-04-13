6832c25f370f6869f2414d3d2c7c9ffd81e0709f
Nimrod <nleonc14@gmail.com>
2023-04-13 16:10:05 -0500

    crear y listar items orden de reparación

--- src/Nebula/Controllers/Taller/ItemRepairOrderController.cs
+++ src/Nebula/Controllers/Taller/ItemRepairOrderController.cs
@@ -1,11 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Nebula.Database.Helpers;
+using Nebula.Plugins.Taller.Models;
+using Nebula.Plugins.Taller.Services;
 
-namespace Nebula.Controllers.Taller
+namespace Nebula.Controllers.Taller;
+
+[Authorize(Roles = AuthRoles.User)]
+[Route("api/taller/[controller]")]
+[ApiController]
+public class ItemRepairOrderController : ControllerBase
 {
-    [Route("api/taller/[controller]")]
-    [ApiController]
-    public class ItemRepairOrderController : ControllerBase
-    {
+    private readonly TallerItemRepairOrderService _itemRepairOrderService;
 
+    public ItemRepairOrderController(TallerItemRepairOrderService itemRepairOrderService)
+    {
+        _itemRepairOrderService = itemRepairOrderService;
+    }
+
+    [HttpGet("Index/{id}")]
+    public async Task<IActionResult> Index(string id)
+    {
+        var itemsRepairOrder = await _itemRepairOrderService.GetItemsRepairOrder(id);
+        return Ok(itemsRepairOrder);
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create([FromBody] TallerItemRepairOrder model)
+    {
+        await _itemRepairOrderService.CreateAsync(model);
+        return Ok(model);
     }
 }
--- src/Nebula/Plugins/Taller/Services/TallerItemRepairOrderService.cs
+++ src/Nebula/Plugins/Taller/Services/TallerItemRepairOrderService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using Nebula.Database;
 using Nebula.Database.Services;
 using Nebula.Plugins.Taller.Models;
@@ -12,5 +13,16 @@ public class TallerItemRepairOrderService : CrudOperationService<TallerItemRepai
 {
     public TallerItemRepairOrderService(IOptions<DatabaseSettings> options) : base(options)
     {
+    }
+
+    /// <summary>
+    /// Obtener lista de Items de la orden de reparación.
+    /// </summary>
+    /// <param name="id">Id Orden Reparación</param>
+    /// <returns>Lista de Items - Orden de Reparación</returns>
+    public async Task<List<TallerItemRepairOrder>> GetItemsRepairOrder(string id)
+    {
+        var filter = Builders<TallerItemRepairOrder>.Filter.Eq(x => x.TallerRepairOrderId, id);
+        return await _collection.Find(filter).ToListAsync();
     }
 }
