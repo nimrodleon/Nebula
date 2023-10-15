2a1744f700815571502b082296972efe7526ef5f
Geison Léon <gleonc09@gmail.com>
2023-10-15 18:59:03 -0500

    payment method agregado

--- src/Nebula/Modules/Sales/Models/InvoiceSale.cs
+++ src/Nebula/Modules/Sales/Models/InvoiceSale.cs
@@ -7,6 +7,7 @@ public class InvoiceSale : BaseSale
 {
     public string TipoOperacion { get; set; } = "0101";
     public string FecVencimiento { get; set; } = string.Empty;
+    public string PaymentMethod { get; set; } = string.Empty;
     public PaymentTerms FormaPago { get; set; } = new PaymentTerms();
     public List<Cuota> Cuotas { get; set; } = new List<Cuota>();
     public string Remark { get; set; } = string.Empty;
