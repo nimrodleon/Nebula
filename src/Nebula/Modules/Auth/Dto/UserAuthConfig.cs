96827ca6fea7923fc58992f5c6f8049fb42dc37e
Nimrod León <nleonc14@gmail.com>
2024-05-29 17:18:45 -0500

    fix: userData

---
+++ src/Nebula/Modules/Auth/Dto/UserAuthConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Nebula.Modules.Auth.Dto;
+
+public class UserAuthConfig
+{
+    public UserAuth UserAuth { get; set; } = new UserAuth();
+    public string CompanyName { get; set; } = string.Empty;
+    public bool IsEnableModInventario { get; set; } = false;
+    public bool IsEnableModComprobante { get; set; } = false;
+    public bool IsEnableModCuentaPorCobrar { get; set; } = false;
+    public bool IsEnableModReparaciones { get; set; } = false;
+    public bool IsEnableModCaja { get; set; } = false;
+}
